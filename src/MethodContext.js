fcbbde0abf424525172bb35de5124a06672ce736
YUURS <20yurakurin04@mail.ru>
2025-04-14 12:24:37 +0300

    task-method-context commit

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -9,17 +9,18 @@ import { EventEmitter } from './EventEmitter';
 export const obj = {
     count: 0,
 
-    clickHandler: function() {
-        this.count += 1
-    },
-
     subscribe() {
-        EventEmitter.on('click', this.clickHandler.bind(this))
+        this._clickHandler = () => {
+            this.count += 1;
+        };
+        EventEmitter.on('click', this._clickHandler);
     },
-
+    
     unsubscribe() {
-        EventEmitter.off('click', this.clickHandler.bind(this))
-    },
+        if (this._clickHandler) {
+            EventEmitter.off('click', this._clickHandler);
+        }
+    }
 };
 
 
@@ -30,7 +31,10 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        args = args.reverse()
+        return this.second(...args)
+    },
     second() {
         // здесь ничего писать не нужно
     },
